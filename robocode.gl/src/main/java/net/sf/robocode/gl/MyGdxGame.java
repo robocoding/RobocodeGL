4a0450466520474fc5851a28ce1568b7523cf3b2
bumfo <bumfo@users.noreply.github.com>
2019-12-16 00:09:21 +0800

    reset window size

--- robocode.gl/src/main/java/net/sf/robocode/gl/MyGdxGame.java
+++ robocode.gl/src/main/java/net/sf/robocode/gl/MyGdxGame.java
@@ -3,6 +3,8 @@ package net.sf.robocode.gl;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -98,6 +100,54 @@ public final class MyGdxGame extends ApplicationAdapter {
 		stage.addActor(new BulletsActor());
 
 		resize();
+
+		Gdx.input.setInputProcessor(new InputProcessor() {
+			@Override
+			public boolean keyDown(int keycode) {
+				if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) || Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) {
+					if (keycode == Input.Keys.I) {
+						Gdx.graphics.setWindowedMode((int) worldWidth, (int) worldHeight);
+					}
+				}
+
+				return false;
+			}
+
+			@Override
+			public boolean keyUp(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyTyped(char character) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				return false;
+			}
+
+			@Override
+			public boolean mouseMoved(int screenX, int screenY) {
+				return false;
+			}
+
+			@Override
+			public boolean scrolled(int amount) {
+				return false;
+			}
+		});
 	}
 
 	private void resize() {
